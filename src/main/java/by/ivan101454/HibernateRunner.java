f71d2e4147f4176f44d9903d410f16777f32c40f
Ivan Sapozhnikov <ivan101454@gmail.com>
2024-06-18 21:30:39 +0300

    add session

--- src/main/java/by/ivan101454/HibernateRunner.java
+++ src/main/java/by/ivan101454/HibernateRunner.java
@@ -1,23 +1,30 @@
 package by.ivan101454;
 
+import by.ivan101454.entities.Transaction;
 import by.ivan101454.entities.Visit;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class HibernateRunner {
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
+//        configuration.addAnnotatedClass(Visit.class);
+//        configuration.addAnnotatedClass(Transaction.class);
         configuration.configure();
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            session.beginTransaction();
             System.out.println("OK");
+            session.beginTransaction();
             Visit visit = Visit.builder()
+                    .visitId(10)
                     .customerId(54)
-                    .visitId(9)
                     .build();
-            session.save(visit);
+            session.persist(visit);
+//            var result = session.createQuery("select v.visitId from Visit v", Visit.class).list();
             session.getTransaction().commit();
         }
     }
